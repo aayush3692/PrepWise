48ed23a734f8679acdf90a16b1c555314b29309e
Aayush Singh <aayushsingh1239@gmail.com>
2025-03-28 13:43:14 +0545

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: "Ai powered software for interview preparation",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.variable} ${monaSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
